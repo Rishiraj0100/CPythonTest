5fc1297ec0cbfdd9fbe838c032fd42875ee9656d
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-10-29 12:23:35 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ ls = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 for i in range(5):
   v += ls[random.randint(0, len(ls)-1)]
 
-ext = Extension(name="hello", sources=["tcy/__init__.pyx"])
-setup(ext_modules=cythonize(ext), version=v,)
+ext = Extension(name="tcy", sources=["tcy/__init__.pyx"])
+setup(ext_modules=cythonize(ext), version=v,name="tcy")
