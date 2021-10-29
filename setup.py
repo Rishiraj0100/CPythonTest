6699864eb0a4d0fbf0c81db011d9be7861773d63
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-10-29 12:46:01 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import random
 
 from distutils.core import Extension, setup
-from Cython.Build import cythonize
+from Cython.Build import cythonize as c
 
 
 v = "0.0.1a"
@@ -11,4 +11,5 @@ for i in range(5):
   v += ls[random.randint(0, len(ls)-1)]
 
 ext = Extension(name="tcy", sources=["tcy/__init__.pyx",])
-setup(ext_modules=cythonize(ext), version=v,name="tcy")
+ext2 = Extension(name="tcy.hi", sources=["tcy/hi.pyx",])
+setup(ext_modules=[c(ext), c(ext2)], version=v,name="tcy")
