6c0c7992192dcb8618345346761c0dc6846479c8
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-10-29 10:06:50 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
+import random
+
 from setuptools import setup
 from Cython.Build import cythonize
 
+
+v = "0.0.1a"
+ls = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+for i in range(5):
+  v += ls.index(random.randint(0, len(ls)-1))
+
 setup(
     name = "_tcy",
+    version = v
     ext_modules = cythonize("tcy/*.py"),
     zip_safe = False
 )
