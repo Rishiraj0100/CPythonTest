627ecbc790f06706b2a15a3b04acc6827df95104
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-10-29 10:27:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 import random
+import numpy as np
 
-from setuptools import setup
+from setuptools import setup, extension
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
+
+
+SRC_DIR="tcy"
+e = Extension(SRC_DIR + ".hi",
+                  [SRC_DIR + "/hi.pyx"],
+                  libraries=[],
+                  include_dirs=[np.get_include()])
 
 
 v = "0.0.1a"
@@ -14,7 +22,7 @@ for i in range(5):
 setup(
     name = "_tcy",
     version = v,
-    ext_modules = cythonize("tcy/*.pyx"),
+    ext_modules = [e],
     zip_safe = False,
     cmdclass={"build_ext": build_ext}
 )
