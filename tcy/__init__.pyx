9b9829cefc3dd24898577c6b65ae718c1680eb74
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-10-29 13:12:48 +0530

    Update __init__.pyx

--- tcy/__init__.pyx
+++ tcy/__init__.pyx
@@ -1,7 +1,11 @@
 from libc.math cimport pow
+from tcy.hi cimport hi
 
 cdef double square_and_add (double x):
     return pow(x, 2.0) + x
 
 cpdef print_result (double x):
     print("({} ^ 2) + {} = {}".format(x, x, square_and_add(x)))
+
+cpdef hi ():
+  return 'hi'
