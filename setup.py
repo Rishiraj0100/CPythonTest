1f112975fb225ef2c838ccd4a4806addd01cbc8a
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-10-29 10:23:12 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import random
 
 from setuptools import setup
 from Cython.Build import cythonize
+from Cython.Distutils import build_ext
 
 
 v = "0.0.1a"
@@ -14,5 +15,6 @@ setup(
     name = "_tcy",
     version = v,
     ext_modules = cythonize("tcy/*.pyx"),
-    zip_safe = False
+    zip_safe = False,
+    cmdclass={"build_ext": build_ext}
 )
