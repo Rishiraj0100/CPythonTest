18e007637f7c0c730c1d0f2033a07d6912b80520
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-11-18 18:24:18 +0530

    Update and rename h.c to t.c

--- t.c
+++ t.c
@@ -2,27 +2,23 @@
 #include <Python.h>
 
 static PyObject *hello(PyObject *self) {
-	return PyUnicode_FromFormat("Hello C extension!");
+  return PyUnicode_FromFormat("Hello C extension!");
 }
-
-char hellofunc_docs[] = "Hello world description.";
 
 static PyMethodDef helloworld_funcs[] = {
   {
     "hello",
     hello,
     METH_NOARGS,
-    hellofunc_docs
+    "Hello world description."
   },
   {NULL, NULL, 0, NULL}
 };
 
-char helloworldmod_docs[] = "This is hello world module.";
-
 static struct PyModuleDef helloworld_mod = {
 	PyModuleDef_HEAD_INIT,
 	"helloworld",
-	helloworldmod_docs,
+	"This is hello world module.",
 	-1,
 	helloworld_funcs
 };
